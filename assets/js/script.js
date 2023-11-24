a52bac2980c217b5f7a879fffc4fa56df71a49d2
trdmonkey <trdmonkey218@gmail.com>
2023-11-24 08:57:06 -0500

    Desarrollo 23112023

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,10 +38,12 @@ const toggleNavbar = function () {
 addEventOnElements(navTogglers, "click", toggleNavbar);
 
 /* 
-* HEADER
+* HEADER & BACK TOP BTN
 */
 
 const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
 let lastScrollPos = 0;
 
 const hideHeader = function () {
@@ -58,9 +60,11 @@ const hideHeader = function () {
 window.addEventListener("scroll", function () {
     if (window.scrollY >= 50) {
         header.classList.add("active");
+        backTopBtn.classList.add("active");
         hideHeader();
     } else {
         header.classList.remove("active");
+        backTopBtn.classList.remove("active");
     }
 });
 
